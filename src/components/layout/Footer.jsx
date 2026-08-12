9c32fe53dd61d0aa090b438c49b48f6551349d6e
Isha <tisha8357@gmail.com>
2026-08-12 13:27:04 +0530

    added correct path to images

--- src/components/layout/Footer.jsx
+++ src/components/layout/Footer.jsx
@@ -1,3 +1,6 @@
+import equinix from "../assets/images/equinix.webp";
+import digitalRealty from "../assets/images/digitalRealty.webp";
+
 const Footer = () => {
   return (
     <footer className="bg-black text-white">
@@ -20,10 +23,29 @@ const Footer = () => {
 
         <div className="mt-10 border-t border-white/20 pt-8">
           <div className="logo-row grid gap-8 text-center sm:grid-cols-2 lg:grid-cols-4">
-            <img src="/src/assets/images/equinix.webp" alt="Equinix" className="mx-auto h-12 w-auto object-contain opacity-90 transition duration-300 hover:-translate-y-1 hover:scale-105" />
-            <img src="/src/assets/images/digitalRealty.webp" alt="Digital Realty" className="mx-auto h-12 w-auto object-contain opacity-90 transition duration-300 hover:-translate-y-1 hover:scale-105" />
-            <img src="/src/assets/images/equinix.webp" alt="Equinix" className="mx-auto h-12 w-auto object-contain opacity-90 transition duration-300 hover:-translate-y-1 hover:scale-105" />
-            <img src="/src/assets/images/digitalRealty.webp" alt="Digital Realty" className="mx-auto h-12 w-auto object-contain opacity-90 transition duration-300 hover:-translate-y-1 hover:scale-105" />
+            <img
+              src={equinix}
+              alt="Equinix"
+              className="mx-auto h-12 w-auto object-contain opacity-90 transition duration-300 hover:-translate-y-1 hover:scale-105"
+            />
+
+            <img
+              src={digitalRealty}
+              alt="Digital Realty"
+              className="mx-auto h-12 w-auto object-contain opacity-90 transition duration-300 hover:-translate-y-1 hover:scale-105"
+            />
+
+            <img
+              src={equinix}
+              alt="Equinix"
+              className="mx-auto h-12 w-auto object-contain opacity-90 transition duration-300 hover:-translate-y-1 hover:scale-105"
+            />
+
+            <img
+              src={digitalRealty}
+              alt="Digital Realty"
+              className="mx-auto h-12 w-auto object-contain opacity-90 transition duration-300 hover:-translate-y-1 hover:scale-105"
+            />
           </div>
         </div>
       </div>
